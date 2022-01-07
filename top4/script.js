409663c7aaa32cb695322865fce25df81fd3b602
SSMEGHANA <50514313+SSMEGHANA@users.noreply.github.com>
2022-01-07 22:54:19 +0530

    Add files via upload

---
+++ top4/script.js
@@ -0,0 +1,65 @@
+const data = {
+  mainTitle: "My Favourite Food Items",
+  cardContents: [
+    {
+      name: "Coffee",
+      imageLink:
+        "https://images.unsplash.com/photo-1447933601403-0c6688de566e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      name: "Pizza",
+      imageLink:
+        "https://images.unsplash.com/photo-1641505396636-bea08f5412d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      name: "Sweet",
+      imageLink:
+        "https://images.unsplash.com/photo-1641339018973-3d9ff9330cb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      name: "Burger",
+      imageLink:
+        "https://images.unsplash.com/photo-1640092132176-2ee6ac96971d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDY0fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+  ],
+  userData: { name: "kitchen.home", tagLine: "Make your dream fulfill" },
+};
+
+
+// Plug in title data to HTML
+let mainTitleElement = document.getElementById("main-title");
+mainTitleElement.innerHTML += data.mainTitle;
+
+
+// Create template for card content
+let getCardStructure = (name, imageLink) => {
+  return `<div class="col-12 col-sm-6 mb-4">
+  <div class="tile">
+    <div class="tile-text text-center">
+      <h5>${name}</h5>
+    </div>
+    <img 
+      src="${imageLink}" />
+  </div>
+</div>`;
+};
+
+let cardRow = document.getElementById("card-row");
+
+// Plug in card data to HTML
+data.cardContents.forEach((cardContent) => {
+  let { name, imageLink } = cardContent;
+  cardRow.innerHTML += getCardStructure(name, imageLink);
+});
+
+// Create template for footer content
+let getFooterStructure = (name, tagLine) => {
+  return `<h3 class="me-3"><i class="fas fa-star"></i> ${name}</h3>
+    <p>${tagLine}</p>`;
+};
+
+let footerContent = document.getElementById("footer");
+let { name, tagLine } = data.userData;
+
+// Plug in footer data to HTML
+footerContent.innerHTML += getFooterStructure(name, tagLine);
